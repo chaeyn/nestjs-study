c4fb01d5212afd584ed2e3d6b55945820914ebc4
chaeyn <chaulfe@gmail.com>
2025-09-06 12:11:51 +0900

    Architecture of NestJs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
+  // url을 가져오고 함수를 실행
   providers: [AppService],
 })
+// 모듈은 @Module 데코레이터로 주석이 달린 클래스
+// 모듈은 Nest가 애플리케이션 구조를 구성하는데 사용되는 메타데이터 제공
 export class AppModule {}
