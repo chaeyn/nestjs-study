beba817c02dd269c7a75959b1e6e2b4ed24141b2
chaeyn <chaulfe@gmail.com>
2025-09-06 13:09:26 +0900

    Separate controller and service, Create fake DB (entities), Add movies CRUD features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
   controllers: [MoviesController],
   // url을 가져오고 함수를 실행
-  providers: [],
+  providers: [MoviesService],
 })
 // 모듈은 @Module 데코레이터로 주석이 달린 클래스
 // 모듈은 Nest가 애플리케이션 구조를 구성하는데 사용되는 메타데이터 제공
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,41 +9,38 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies') // entry point -> /movies
 export class MoviesController {
-  @Get()
-  getAll(): string {
-    return '모든 영화를 가져옵니다.';
-  }
+  constructor(private readonly moviesService: MoviesService) {}
+  // constructor 메서드는 클래스의 인스턴스 객체를 생성, 초기화하는 메서드
 
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `${searchingYear}년 이후의 영화를 검색합니다`;
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
+  getOne(@Param('id') movieId: string): Movie {
     // id를 파라미터로 받아옴
-    return `${movieId}번 영화를 가져옵니다.`;
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `${movieId}번 영화를 삭제합니다.`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
   patch(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+    return this.moviesService.update(movieId, updateData);
   }
 }
 
